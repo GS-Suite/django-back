4f5e337b0e2980f6cff8c5dcf937644629084045
keane3pereira <keane3pereira@gmail.com>
2020-12-18 09:01:18 +0530

    user basic

--- user/managers.py
+++ user/managers.py
@@ -3,21 +3,23 @@ from django.utils.translation import ugettext_lazy as _
 
 #model manager file (created)
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError(_('Email must be set'))
-        user = self.model(email = email, **extra_fields)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError(_('Username must be set'))
+        user = self.model(username = username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
         
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
