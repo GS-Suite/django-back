2d67354b25e8f2d6335d03df0aa4d999d145894d
keane3pereira <keane3pereira@gmail.com>
2020-12-22 12:41:04 +0530

    sign up

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token, verify_jwt_token
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path
+import user.views as user_views
 
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('auth/jwt/', obtain_jwt_token, name="new_token"),              # returns token
     path('auth/jwt_verify/', verify_jwt_token, name="verify_token"),    # returns token
     #path('auth/jwt_refresh/', refresh_jwt_token, name="refresh_token"), 
+
+    path('sign_up/', user_views.SignUp.as_view(), name="sign_up")
 ]
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from user.models import CustomUser
+from user.forms import CustomUserCreationForm
+import json
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class SignUp(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.data)
+
+        if form.is_valid():
+            form.save()
+        
+        return Response(form.is_valid())
+
+        '''
+        {
+            "username": "abc"
+            "password1": "enaek123",
+            "password2": "enaek123",
+            "name": "Keane"
+        }
+        '''
